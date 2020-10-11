674a713a2a28886d185baba104a3be24e54a95a4
Rashad Goddard <rashadjgoddard@gmail.com>
2020-10-11 13:55:02 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -4,11 +4,13 @@ describe "say_hello" do
   
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
-    say_hello("Kent Beck")
+    say_hello("Kent Beck")do
   end
+end
+
 
   it 'defaults to Ruby Programmer when no name is passed in' do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-    say_hello()
+    'say_hello(name)'
   end
 end
